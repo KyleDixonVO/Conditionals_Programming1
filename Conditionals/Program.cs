4cbfe7e67dd1167a09afce5a14e9c4b88b97561e
KyleDixonVO <KyleDixon@hotmail.ca>
2021-10-04 12:30:23 -0300

    Update Program.cs

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -8,8 +8,44 @@ namespace Conditionals
 {
     class Program
     {
+        static int score = 0;
+        static int lives = 3;
+        static bool oneUpGained = false;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Conditionals!");
+            ShowHUD();
+            
+           
+            //if, then, else, etc.
+            score = score + 100;
+            ShowHUD();
+            score = score + 100;
+            ShowHUD();
+            score = score + 1000000;
+
+            if (oneUpGained == false)
+            {
+                if (score >= 1000000)
+                {
+                    lives = lives + 1;
+                    oneUpGained = true;
+                }
+            }
+
+          
+            ShowHUD();
+
+        
+            Console.ReadKey(true);
+        }
+
+        static void ShowHUD()
+        {
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Lives: " + lives);
+
         }
     }
 }
